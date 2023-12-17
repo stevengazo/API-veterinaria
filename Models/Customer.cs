828c7abf2d6798dfb0c9be010f55cc0824d0709c
Steven Gazo M <steven.gazo@hotmail.com>
2023-12-17 00:53:56 -0600

    post customers

--- Models/Customer.cs
+++ Models/Customer.cs
@@ -10,7 +10,7 @@ public class Customer : Person
     public string UserName { get; set; }
     public string HashPassword { private get;  set; }
     #region Relations
-    public ICollection<Animal> Animals { get; set; }
+    public ICollection<Animal>? Animals { get; set; }
 
 
 
