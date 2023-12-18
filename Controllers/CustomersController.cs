2f1d732017e717c21fb104f727fe44f7e2ec7e8b
Steven Gazo M <steven.gazo@hotmail.com>
2023-12-18 16:48:47 -0600

    customer password

--- Controllers/CustomersController.cs
+++ Controllers/CustomersController.cs
@@ -50,6 +50,52 @@ namespace API.Controllers
             return customer;
         }
 
+// PUT: api/Veterinarian/5 Este put modifica solamente la contraseña
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("EditProfilePassword/{id}")]
+        public async Task<IActionResult> PutClinicPassword(int id, Customer customer)
+        {
+            if (id != customer.CustomerId)
+            {
+                return BadRequest();
+                //return NotFound();
+            }
+ 
+            // Evitar que se modifique el campo hashPassword
+            var existingCustomer = await _context.Customers.FindAsync(id);
+ 
+            if (existingCustomer == null)
+            {
+                return NotFound();
+            }
+ 
+            // Copiar los valores permitidos desde el objeto enviado al objeto existente
+            //existingClinic.ClinicId = clinic.ClinicId;
+            existingCustomer.HashPassword = customer.HashPassword;
+ 
+            _context.Customers.Update(existingCustomer);
+ 
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CustomerExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+ 
+            return NoContent();
+        }
+ 
+
         [HttpGet("{UserName}")]
         public async Task<ActionResult<Customer>> GetCustomerUsername(String UserName)
         {
